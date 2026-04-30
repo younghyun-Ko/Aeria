aa95c729d46db77345c9f88acf91836513a6ca54
younghyun-Ko <younghyun032578@gmail.com>
2026-04-30 12:21:35 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // eslint 검사 무시
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // typescript 에러 무시
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig; // 만약 파일명이 .mjs면 export default nextConfig; 로 쓰셈
+export default nextConfig;
